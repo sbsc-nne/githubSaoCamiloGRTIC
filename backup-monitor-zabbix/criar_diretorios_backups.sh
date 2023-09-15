9b8f83aae25f080bb408c0d54be0e4e568b3a19f
mgran2003 <marcelo.grando@gmail.com>
2023-09-15 14:06:41 -0300

    Update criar_diretorios_backups.sh

--- backup-monitor-zabbix/criar_diretorios_backups.sh
+++ backup-monitor-zabbix/criar_diretorios_backups.sh
@@ -1,5 +1,24 @@
-mkdir /mnt/bkp_fortes_ac
-mkdir /mnt/bkp_fortes_ponto
-mkdir /mnt/bkp_fortes_rh
-mkdir /mnt/bkp_systemah2005
-mkdir /mnt/bkp_wk_sistemas
+#!/bin/bash
+# Data Criação: 01-08-2022
+# @Marcelo Grando
+# Script 
+
+if [! -d "/mnt/bkp_fortes_ac"]; then
+    mkdir /mnt/bkp_fortes_ac
+fi
+
+if [! -d "/mnt/bkp_fortes_ponto"]; then
+    mkdir /mnt/bkp_fortes_ponto
+fi
+
+if [! -d "/mnt/bkp_fortes_rh"]; then
+    mkdir /mnt/bkp_fortes_rh
+fi
+
+if [! -d "/mnt/bkp_systemah2005"]; then
+    mkdir /mnt/bkp_systemah2005
+fi
+
+if [! -d "/mnt/bkp_wk_sistemas"]; then
+    mkdir /mnt/bkp_wk_sistemas
+fi
