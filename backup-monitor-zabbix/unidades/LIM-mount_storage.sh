262582ad5c9e1c3ffc3fedcd5542882907df3b41
mgran2003 <marcelo.grando@gmail.com>
2023-09-21 16:09:00 -0300

    Update Userparameter Limoeiro

--- backup-monitor-zabbix/unidades/LIM-mount_storage.sh
+++ backup-monitor-zabbix/unidades/LIM-mount_storage.sh
@@ -5,6 +5,6 @@
 
 mount -t cifs //10.1.0.43/Backup_Fortes_AC -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0444,file_mode=0444,nounix  /mnt/bkp_fortes_ac
 mount -t cifs //10.1.0.43/Backup_Fortes_Ponto -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0444,file_mode=0444,nounix  /mnt/bkp_fortes_ponto
-mount -t cifs //10.1.0.43/Backup_Bancos/Fortes_RH -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0444,file_mode=0444,nounix  /mnt/bkp_fortes_rh
-mount -t cifs //10.1.0.43/Backup_Bancos/Systema -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0444,file_mode=0444,nounix  /mnt/bkp_systemah2005
+mount -t cifs //10.1.0.43/Backup_Fortes_RH -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0444,file_mode=0444,nounix  /mnt/bkp_fortes_rh
+mount -t cifs //10.1.0.43/Backup_Systemah -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0444,file_mode=0444,nounix  /mnt/bkp_systemah2005
 mount -t cifs //10.1.0.43/Backup_WK -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0444,file_mode=0444,nounix  /mnt/bkp_wk_sistemas
