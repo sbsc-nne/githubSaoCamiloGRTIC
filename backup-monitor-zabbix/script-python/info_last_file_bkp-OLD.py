d8dd438f25b7a25c4b397d307676630ab05a278b
mgran2003 <marcelo.grando@gmail.com>
2023-09-21 16:49:26 -0300

    Arquivo correto info_last_file_bkp.py

--- backup-monitor-zabbix/script-python/info_last_file_bkp-OLD.py
+++ backup-monitor-zabbix/script-python/info_last_file_bkp-OLD.py
@@ -1,25 +1,29 @@
 # Data Criação: 14-03-2022
 # @Marcelo Grando
-# Script com as funções com informação do ultimo arquivo criado em uma determinada pasta.
+# Script com funções para retornar informações do ultimo arquivo criado em uma pasta específica
+# Para usar as funções deve-se crir novos arquivos .py fazendo import, chamando a função e passando o(path) como parametro.
 
 import os
 from datetime import datetime
-def last_file_name(path,extension): # Listar o nome do ultimo arquivo criado em uma determinada pasta (path)
+
+
+def last_file_name(path): # Listar o nome do ultimo arquivo criado em uma determinada pasta (path)
     lista_arquivos = os.listdir(path)
     lista_datas = []
     for arquivo in lista_arquivos:
-        if extension in arquivo:
+        if ".py" in arquivo:
             data = os.path.getmtime(f"{path}/{arquivo}")
             lista_datas.append((data, arquivo))
     lista_datas.sort(reverse=True)
     ultimo_arquivo = lista_datas[0]
     print(ultimo_arquivo[1])
 
-def last_file_date(path,extension): # Listar a data de criação do ultimo arquivo criado em uma determinada pasta (path)
+
+def last_file_date(path): # Listar a data de criação do ultimo arquivo criado em uma determinada pasta (path)
     lista_arquivos = os.listdir(path)
     lista_datas = []
     for arquivo in lista_arquivos:
-        if extension in arquivo:
+        if ".py" in arquivo:
             data = os.path.getmtime(f"{path}/{arquivo}")
             lista_datas.append((data, arquivo))
     lista_datas.sort(reverse=True)
@@ -28,11 +32,12 @@ def last_file_date(path,extension): # Listar a data de criação do ultimo arqui
     ultima_data = datetime.fromtimestamp(data).strftime('%d/%m/%Y %H:%M')
     print(ultima_data)
 
-def last_file_size(path,extension): # Listar o tamanho em kb do ultimo arquivo criado em uma determinada pasta (path)
+
+def last_file_size(path): # Listar o tamanho em kb do ultimo arquivo criado em uma determinada pasta (path)
     lista_arquivos = os.listdir(path)
     lista_datas = []
     for arquivo in lista_arquivos:
-        if extension in arquivo:
+        if ".py" in arquivo:
             data = os.path.getmtime(f"{path}/{arquivo}")
             lista_datas.append((data, arquivo))
     lista_datas.sort(reverse=True)
@@ -40,4 +45,4 @@ def last_file_size(path,extension): # Listar o tamanho em kb do ultimo arquivo c
     f_path = os.path.join(path, ultimo_arquivo[1])
     f_size = os.path.getsize(f_path)
     f_size_kb = f_size / 1024  # obter resultado em kB
-    print(f'{f_size_kb:.2f}')
+    print(f'{f_size_kb:.2f} kb')
