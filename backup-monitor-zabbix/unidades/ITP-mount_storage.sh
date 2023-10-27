8b7b4791a497d23b96136eeff4135074eedce7a5
mgran2003 <marcelo.grando@gmail.com>
2023-10-27 13:34:59 -0300

    Config Itapipoca

---
+++ backup-monitor-zabbix/unidades/ITP-mount_storage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Data Criação: 01-08-2022
+# Data Alteração: 28-09-2023
+# @Marcelo Grando
+# Script 
+
+mount -t cifs //192.168.0.36/Backup_FortesAC -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_ac
+mount -t cifs //192.168.0.36/Backup_FortesPonto -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_ponto
+mount -t cifs //192.168.0.36/Backup_FortesRH -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_rh
+mount -t cifs //192.168.0.36/Backup_SystemaH -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_systemah2005
+mount -t cifs //192.168.0.36/Backup_WKSistemas -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_wk_sistemas
