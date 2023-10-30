a8fd70e836804036f4504d01a5e323920c7d4e05
mgran2003 <marcelo.grando@gmail.com>
2023-10-30 09:19:02 -0300

    Config Tianguá

---
+++ backup-monitor-zabbix/unidades/TNG-mount_storage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Data Criação: 01-08-2022
+# @Marcelo Grando
+# Script 
+
+mount -t cifs //192.168.1.101/Backup_FortesAC -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_ac
+mount -t cifs //192.168.1.101/Backup_FortesPonto -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_ponto
+mount -t cifs //192.168.1.101/Backup_FortesRH -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_rh
+mount -t cifs //192.168.1.101/Backup_SystemaH -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_systemah2005
+mount -t cifs //192.168.1.101/Backup_WKSistemas -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_wk_sistemas
