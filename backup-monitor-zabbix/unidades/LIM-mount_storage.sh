f034a6fd09bd46526405e91c6624ce43683b606c
mgran2003 <marcelo.grando@gmail.com>
2023-11-16 10:49:25 -0300

    Config Limoeiro

--- backup-monitor-zabbix/unidades/LIM-mount_storage.sh
+++ backup-monitor-zabbix/unidades/LIM-mount_storage.sh
@@ -3,8 +3,8 @@
 # @Marcelo Grando
 # Script 
 
-mount -t cifs //10.1.0.43/Backup_Fortes_AC -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_ac
-mount -t cifs //10.1.0.43/Backup_Fortes_Ponto -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_ponto
-mount -t cifs //10.1.0.43/Backup_Fortes_RH -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_rh
+mount -t cifs //10.1.0.43/Backup_FortesAC -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_ac
+mount -t cifs //10.1.0.43/Backup_FortesPonto -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_ponto
+mount -t cifs //10.1.0.43/Backup_FortesRH -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_fortes_rh
 mount -t cifs //10.1.0.43/Backup_Systemah -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_systemah2005
-mount -t cifs //10.1.0.43/Backup_WK -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_wk_sistemas
+mount -t cifs //10.1.0.43/Backup_WKSistemas -o 'username=zabbix','password=z4bb1x!@2022',dir_mode=0555,file_mode=0555,nounix  /mnt/bkp_wk_sistemas
