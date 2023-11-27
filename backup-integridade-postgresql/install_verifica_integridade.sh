47fbc80027f9050a450787c8aacf2e12eecce1e6
mgran2003 <marcelo.grando@gmail.com>
2023-11-27 11:42:12 -0300

    Update name file userparameter

--- backup-integridade-postgresql/install_verifica_integridade.sh
+++ backup-integridade-postgresql/install_verifica_integridade.sh
@@ -17,7 +17,7 @@ echo '##### Download do arquivo... #####'
 
 wget -O /home/check_db_pgsql/verificar_integridade_postgresql.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-integridade-postgresql/verificar_integridade_postgresql.sh
 wget -O /home/check_db_pgsql/ler_log_integridade_postgresql.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-integridade-postgresql/ler_log_integridade_postgresql.sh
-wget -O /etc/zabbix/zabbix_agentd.d/userparameter_integridade_postgresql.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-integridade-postgresql/userparameter_integridade_postgresql.sh
+wget -O /etc/zabbix/zabbix_agentd.d/userparameter_integridade_postgresql.conf https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-integridade-postgresql/userparameter_integridade_postgresql.conf
 
 chmod +x /home/check_db_pgsql/verificar_integridade_postgresql.sh
 chmod +x /home/check_db_pgsql/ler_log_integridade_postgresql.sh
