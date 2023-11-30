9b52908698caf71210e3f1025526fabd6a5da694
mgran2003 <marcelo.grando@gmail.com>
2023-11-30 17:36:30 -0300

    Update create structure and task_check_version

--- version_files_exe/create_structure_version_files.ps1
+++ version_files_exe/create_structure_version_files.ps1
@@ -8,14 +8,29 @@
 #$fullPathFileDefault = 'C:\zabbix_agent2\check_version_files\version_files_teste.ps1'
 $directoryDefault = "C:\zabbix_agent2\check_version_files"  #Split-Path $fullPathFileDefault -Parent
 $urlFileConfig = "https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/version_files_exe/version_files.ps1"
+$urlFileTaskXML = "https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/version_files_exe/task_check_version_files.xml"
 
 # Criar um diretório
 if (-not (Test-Path -Path $directoryDefault -PathType Container)) {
     New-Item -Path $directoryDefault -ItemType Directory
 }
 
-# Nome do arquivo local (extraído do URL)
+# Nome do arquivo local (extraído do URL) $urlFileConfig
 $nameLocalFile = Join-Path $directoryDefault (Split-Path $urlFileConfig -Leaf)
 
-# Baixa o arquivo e salva localmente
+# Baixa o arquivo e salva localmente $urlFileConfig
 Invoke-WebRequest -Uri $urlFileConfig -OutFile $nameLocalFile
+
+# Nome do arquivo local (extraído do URL) $urlFileTaskXML
+$nameLocalFile = Join-Path $directoryDefault (Split-Path $urlFileTaskXML -Leaf)
+
+# Baixa o arquivo e salva localmente $urlFileTaskXML
+Invoke-WebRequest -Uri $urlFileTaskXML -OutFile $nameLocalFile
+
+$pathFullXml = Join-Path -Path $directoryDefault -ChildPath $nameLocalFile
+
+# Caminho para o arquivo XML que contém a definição da tarefa agendada
+$pathFullXml = "C:\Caminho\Para\Seu\Arquivo.xml"
+
+# Importa a tarefa agendada a partir do XML
+Register-ScheduledTask -Action (New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-File $pathFullXml") -TaskName "check_version_files_2"
