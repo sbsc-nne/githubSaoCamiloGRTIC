b0a6655585d54b994a3f4e80acc5040211e9f7a7
mgran2003 <marcelo.grando@gmail.com>
2023-11-30 17:18:28 -0300

    Update create_structure_version_files.ps1

--- version_files_exe/create_structure_version_files.ps1
+++ version_files_exe/create_structure_version_files.ps1
@@ -5,11 +5,17 @@
 # Script estruturar a verificação das versões de arquivos EXE (Monitoramento Zabbix)
 
 # Variáveis
-$fullPathFileDefault = 'C:\zabbix_agent2\check_version_files\version_files_teste.ps1'
-$directoryDefault = Split-Path $fullPathFileDefault -Parent
-$urlFileConfig = ""
+#$fullPathFileDefault = 'C:\zabbix_agent2\check_version_files\version_files_teste.ps1'
+$directoryDefault = "C:\zabbix_agent2\check_version_files"  #Split-Path $fullPathFileDefault -Parent
+$urlFileConfig = "https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/version_files_exe/version_files.ps1"
 
 # Criar um diretório
 if (-not (Test-Path -Path $directoryDefault -PathType Container)) {
     New-Item -Path $directoryDefault -ItemType Directory
 }
+
+# Nome do arquivo local (extraído do URL)
+$nameLocalFile = Join-Path $directoryDefault (Split-Path $urlFileConfig -Leaf)
+
+# Baixa o arquivo e salva localmente
+Invoke-WebRequest -Uri $urlFileConfig -OutFile $nameLocalFile
