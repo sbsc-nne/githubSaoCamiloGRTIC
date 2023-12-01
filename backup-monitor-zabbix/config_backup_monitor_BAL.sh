b774f722f6adda2fdfd135bf8057cf5aa72c307b
mgran2003 <marcelo.grando@gmail.com>
2023-12-01 13:43:38 -0300

    Update path scripts

--- backup-monitor-zabbix/config_backup_monitor_BAL.sh
+++ backup-monitor-zabbix/config_backup_monitor_BAL.sh
@@ -5,7 +5,7 @@
 
 echo '##### Iniciando configuração #####'
 
-DIR_OLD="/etc/zabbix/script-python"
+DIR_OLD="/etc/zabbix/script"
 DIR="/etc/zabbix/script"
 
 if [ -d "$DIR_OLD" ]; then
@@ -25,14 +25,14 @@ wget -O /tmp/criar_diretorios_backups.sh https://github.com/mgran2003/GITHUB-SAO
 
 sh /tmp/criar_diretorios_backups.sh
 
-echo '##### Download do arquivos script-python #####'
+echo '##### Download do arquivos script #####'
 
-rm -rf /etc/zabbix/script-python/*
+rm -rf /etc/zabbix/script/*
 
-wget -O /etc/zabbix/script-python/last_file_date.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script-python/last_file_date.sh
-wget -O /etc/zabbix/script-python/last_file_name.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script-python/last_file_name.sh
-wget -O /etc/zabbix/script-python/last_file_size.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script-python/last_file_size.sh
-wget -O /etc/zabbix/script-python/mount_storage.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/unidades/BAL-mount_storage.sh
+wget -O /etc/zabbix/script/last_file_date.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script/last_file_date.sh
+wget -O /etc/zabbix/script/last_file_name.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script/last_file_name.sh
+wget -O /etc/zabbix/script/last_file_size.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script/last_file_size.sh
+wget -O /etc/zabbix/script/mount_storage.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/unidades/BAL-mount_storage.sh
 
 
 echo '##### Download do arquivos zabbix_agentd.d userparameter #####'
@@ -46,6 +46,6 @@ wget -O $fileUserParameter https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/r
 wget -O /etc/zabbix/zabbix_agentd.d/userparameter_size_used_storage.conf https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/zabbix_agentd.d/userparameter_size_used_storage.conf
 
 sleep 3s
-sh /etc/zabbix/script-python/mount_storage.sh
+sh /etc/zabbix/script/mount_storage.sh
 
 echo '##### Finalizado #####'
--- backup-monitor-zabbix/config_backup_monitor_CTO.sh
+++ backup-monitor-zabbix/config_backup_monitor_CTO.sh
@@ -5,7 +5,7 @@
 
 echo '##### Iniciando configuração #####'
 
-DIR="/etc/zabbix/script-python"
+DIR="/etc/zabbix/script"
  
 if [ ! -d "$DIR" ]; then
   mkdir -p $DIR && cd $DIR
@@ -19,13 +19,13 @@ wget -O /tmp/criar_diretorios_backups.sh https://github.com/mgran2003/GITHUB-SAO
 
 sh /tmp/criar_diretorios_backups.sh
 
-echo '##### Download do arquivos script-python #####'
+echo '##### Download do arquivos script #####'
 
-wget -O /etc/zabbix/script-python/info_last_file_bkp.py https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script-python/info_last_file_bkp.py
-wget -O /etc/zabbix/script-python/last_file_date.py https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script-python/last_file_date.py
-wget -O /etc/zabbix/script-python/last_file_name.py https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script-python/last_file_name.py
-wget -O /etc/zabbix/script-python/last_file_size.py https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script-python/last_file_size.py
-wget -O /etc/zabbix/script-python/mount_storage.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/unidades/CTO-mount_storage.sh
+wget -O /etc/zabbix/script/info_last_file_bkp.py https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script/info_last_file_bkp.py
+wget -O /etc/zabbix/script/last_file_date.py https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script/last_file_date.py
+wget -O /etc/zabbix/script/last_file_name.py https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script/last_file_name.py
+wget -O /etc/zabbix/script/last_file_size.py https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/script/last_file_size.py
+wget -O /etc/zabbix/script/mount_storage.sh https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/unidades/CTO-mount_storage.sh
 
 echo '##### Download do arquivos zabbix_agentd.d userparameter #####'
 
@@ -33,6 +33,6 @@ wget -O /etc/zabbix/zabbix_agentd.d/userparameter_info_last_file_bkp.conf https:
 wget -O /etc/zabbix/zabbix_agentd.d/userparameter_size_used_storage.conf https://github.com/mgran2003/GITHUB-SAOCAMILO-GRTIC/raw/main/backup-monitor-zabbix/zabbix_agentd.d/userparameter_size_used_storage.conf
 
 sleep 3s
-sh /etc/zabbix/script-python/mount_storage.sh
+sh /etc/zabbix/script/mount_storage.sh
 
 echo '##### Finalizado #####'
