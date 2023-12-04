48c81cd9630f12c5cc96d431213ab0af269e7581
mgran2003 <marcelo.grando@gmail.com>
2023-12-04 14:08:23 -0300

    Update validar a leitura dos backups

--- backup-monitor-zabbix/script/validar_backup.sh
+++ backup-monitor-zabbix/script/validar_backup.sh
@@ -10,23 +10,27 @@ if [ -z "$1" ]; then
 fi
 # Parâmetro 1 - IP da maquina do Zabbix Agenr
 IP=$1
+echo "Backup Fortes AC"
+echo "Nome:     $(zabbix_get -s $IP -p 10050 -k bkp_ac_last_file_name) "
+echo "Data:     $(zabbix_get -s $IP -p 10050 -k bkp_ac_last_file_date) "
+echo "Tamanho:  $(zabbix_get -s $IP -p 10050 -k bkp_ac_last_file_size) "
 
-echo "Backup Fortes AC - Nome:      $(zabbix_get -s $IP -p 10050 -k bkp_ac_last_file_name) "
-echo "Backup Fortes AC - Data:      $(zabbix_get -s $IP -p 10050 -k bkp_ac_last_file_date) "
-echo "Backup Fortes AC - Tamanho:   $(zabbix_get -s $IP -p 10050 -k bkp_ac_last_file_size) "
+echo "Backup Fortes PONTO"
+echo "Nome:     $(zabbix_get -s $IP -p 10050 -k bkp_ponto_last_file_name) "
+echo "Data:     $(zabbix_get -s $IP -p 10050 -k bkp_ponto_last_file_date) "
+echo "Tamanho:  $(zabbix_get -s $IP -p 10050 -k bkp_ponto_last_file_size) "
 
-echo "Backup Fortes PONTO - Nome:   $(zabbix_get -s $IP -p 10050 -k bkp_ponto_last_file_name) "
-echo "Backup Fortes PONTO - Data:   $(zabbix_get -s $IP -p 10050 -k bkp_ponto_last_file_date) "
-echo "Backup Fortes PONTO - Tamanho:$(zabbix_get -s $IP -p 10050 -k bkp_ponto_last_file_size) "
+echo "Backup Fortes RH"
+echo "Nome:     $(zabbix_get -s $IP -p 10050 -k bkp_rh_last_file_name) "
+echo "Data:     $(zabbix_get -s $IP -p 10050 -k bkp_rh_last_file_date) "
+echo "Tamanho:  $(zabbix_get -s $IP -p 10050 -k bkp_rh_last_file_size) "
 
-echo "Backup Fortes RH - Nome:      $(zabbix_get -s $IP -p 10050 -k bkp_rh_last_file_name) "
-echo "Backup Fortes RH - Data:      $(zabbix_get -s $IP -p 10050 -k bkp_rh_last_file_date) "
-echo "Backup Fortes RH - Tamanho:   $(zabbix_get -s $IP -p 10050 -k bkp_rh_last_file_size) "
+echo "Backup SystemaH2005"
+echo "Nome:   $(zabbix_get -s $IP -p 10050 -k bkp_systema_last_file_name) "
+echo "Data:   $(zabbix_get -s $IP -p 10050 -k bkp_systema_last_file_date) "
+echo "Tamanho:$(zabbix_get -s $IP -p 10050 -k bkp_systema_last_file_size) "
 
-echo "Backup SystemaH2005 - Nome:   $(zabbix_get -s $IP -p 10050 -k bkp_systema_last_file_name) "
-echo "Backup SystemaH2005 - Data:   $(zabbix_get -s $IP -p 10050 -k bkp_systema_last_file_date) "
-echo "Backup SystemaH2005 - Tamanho:$(zabbix_get -s $IP -p 10050 -k bkp_systema_last_file_size) "
-
-echo "Backup WKSistemas - Nome:     $(zabbix_get -s $IP -p 10050 -k bkp_wk_last_file_name) "
-echo "Backup WKSistemas - Data:     $(zabbix_get -s $IP -p 10050 -k bkp_wk_last_file_date) "
-echo "Backup WKSistemas - Tamanho:  $(zabbix_get -s $IP -p 10050 -k bkp_wk_last_file_size) "
+echo "Backup WKSistemas"
+echo "Nome:     $(zabbix_get -s $IP -p 10050 -k bkp_wk_last_file_name) "
+echo "Data:     $(zabbix_get -s $IP -p 10050 -k bkp_wk_last_file_date) "
+echo "Tamanho:  $(zabbix_get -s $IP -p 10050 -k bkp_wk_last_file_size) "
