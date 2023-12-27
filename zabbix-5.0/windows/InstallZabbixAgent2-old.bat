473f5dcb48abbe62047509b29047356bed80bac7
mgran2003 <marcelo.grando@gmail.com>
2023-12-27 14:45:45 -0300

    Delete File

--- zabbix-5.0/windows/InstallZabbixAgent2-old.bat
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-echo "== Ocultar a pasta do Zabbix Agent =="
-attrib +s +h "C:\zabbix"
-echo "== Instalando Zabbix Agent2 como serviço. =="
-C:\zabbix\zabbix_agent2.exe -i -c C:\zabbix\zabbix_agent2.conf
-echo "== Iniciando Servico Zabbix Agent2 =="
-
-net start "Zabbix Agent 2"
